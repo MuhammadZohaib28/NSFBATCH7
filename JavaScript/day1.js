1f3187b38d30110cd0a57f512f6ac24163ab2b1c
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2025-03-25 23:21:18 +0500

    JS DAY 1

---
+++ JavaScript/day1.js
@@ -0,0 +1,18 @@
+console.log("Hello, World 123 @"); //Printing Statement
+
+//STRING ABCD1223S@#ASASD
+//NUMBER 12123123123242312
+
+console.log(300 + 100, "ADDITION")  //ADDITION
+
+console.log(300 - 100, "Substraction")  //SUBTRACTION
+
+console.log("70" + "30", "Concatenation +")  //CONCATENATE   
+
+console.log("70" + 30, "Concatenation -")  //CONCATENATE   
+
+console.log(70 - "10", "Substraction num and strings")  //CONCATENATE
+
+
+console.log("70" - "300", "Subtraction string and string")  //SUBTRACTION
+
