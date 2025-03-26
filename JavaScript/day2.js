5d36d2a6cc6fd354a3cc91037021f833c56ee556
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2025-03-26 22:37:01 +0500

    DAY 2 JS

---
+++ JavaScript/day2.js
@@ -0,0 +1,36 @@
+// VAR CONST LET
+
+console.log("Muhammad Zohaib")
+
+console.log(24)
+
+
+var userName = "Muhammad Zohaib"
+var userAge = 24 + 2
+
+var userName = "Bisma Khan"
+
+//Var is GLOBAL SCOPED
+
+console.log("User name is " + userName + " and user age is " + userAge)
+
+
+
+//const is not global scoped
+{
+    var firstName = "Ahmed"
+
+    const lastName = "Junad"
+
+    console.log(lastName)
+
+}
+
+console.log(firstName, "YEH FIRST NAME HAI")
+
+
+
+{
+    let fatherName = "Javaid Ahmed"
+}
+console.log(fatherName, "yeh let keyword say aarha hai")
